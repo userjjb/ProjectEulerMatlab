14cea7a35d6d893ce4428b8d32febc08db54fc84
Josh Bevan <userjjb@gmail.com>
2014-04-18 22:12:35 -0400

    Application note and symbolic math change
    Added .txt of my Project Euler post explaining the methodology. Also
    changed calculation to use the Symbolic Math Toolbox to accommodate
    arbitrarily long results.

--- Prob5/Euler5.m
+++ Prob5/Euler5.m
@@ -1,5 +1,5 @@
 function [LCM] = Euler5(N)
 %Finds least common multiple on numbers 1 to N
-    P = primes(N); %We should use something faster here really, Matlab's implementation is slooooow.
+    P = sym(primes(N)); %We should use something faster here really, Matlab's implementation is slooooow.
     A = P(P<=sqrt(N));
     LCM = prod(A.^(floor(log(N)./log(A))-1))*prod(P);
